ef09067d8641885e0964e912dee3e44fbbe5c93a
Omkarbade <omakrbade3799@gmail.com>
2022-02-18 01:03:15 +0530

    [Omkar] Add BalancedParentheses program

---
+++ DataStructure/BalancedParentheses.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class BalancedParentheses
+    {
+        
+            public Stack stack = new Stack();
+            public string expression = "(5+6)*(7+8)/(4+3)(5+6)*(7+8)/(4+3)";
+
+            public void BalanceCheck()
+            {
+                char[] arrayOfexpression = expression.ToCharArray();
+
+                for (int i = 0; i < arrayOfexpression.Length; i++)
+                {
+                    if (arrayOfexpression[i] == '(')
+                    {
+                        stack.Push(arrayOfexpression[i]);
+                    }
+                    if (arrayOfexpression[i] == ')')
+                    {
+                        if (stack.top.Equals('('))
+                        {
+                            stack.Pop();
+                        }
+                    }
+
+                }
+
+                if (stack.IsEmpty().Equals(0))
+                {
+                    System.Console.WriteLine("It's Balanced\n ");
+                }
+                else
+                {
+                    System.Console.WriteLine("It's Not Balanced\n ");
+                }
+            }
+    }
+}
---
+++ DataStructure/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BalancedParentheses balancedParentheses = new BalancedParentheses();
+            balancedParentheses.BalanceCheck();
+            Console.ReadLine();
+        }
+    }
+}
